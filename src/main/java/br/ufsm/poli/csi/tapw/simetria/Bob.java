8ac4638ad173e0c4f7138b4b555c75087266374a
Érico <oliv.ericorib@gmail.com>
2022-09-29 15:37:53 -0300

    Testando com arquivos de diferentes tipos

--- src/main/java/br/ufsm/poli/csi/tapw/simetria/Bob.java
+++ src/main/java/br/ufsm/poli/csi/tapw/simetria/Bob.java
@@ -3,12 +3,9 @@ package br.ufsm.poli.csi.tapw.simetria;
 import lombok.SneakyThrows;
 
 import javax.crypto.*;
-import javax.swing.*;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 
 public class Bob {
 
@@ -16,34 +13,34 @@ public class Bob {
     public static void main(String[] args) {
         try (ServerSocket ss = new ServerSocket(5555);) {
             // while (true) {
-                System.out.println("Aguardando conexão na porta 5555...");
+            System.out.println("Aguardando conexão na porta 5555...");
 
-                Socket socket = ss.accept();
-                System.out.println("Conexão recebida!");
+            Socket socket = ss.accept();
+            System.out.println("Conexão recebida!");
 
-                ObjectInputStream oin = new ObjectInputStream(socket.getInputStream());
-                System.out.println("Recebendo arquivo...");
+            ObjectInputStream oin = new ObjectInputStream(socket.getInputStream());
+            System.out.println("Recebendo arquivo...");
 
-                SwitchObject objetoTroca = (SwitchObject) oin.readObject();
-                System.out.println("Arquivo recebido!");
+            SwitchObject objetoTroca = (SwitchObject) oin.readObject();
+            System.out.println("Arquivo recebido!");
 
-                Cipher cipher = Cipher.getInstance("AES");
-                cipher.init(Cipher.DECRYPT_MODE, objetoTroca.getChave());
-                FileOutputStream fout = new FileOutputStream(objetoTroca.getNomeArquivo());
-                FileOutputStream foutCifrado = new FileOutputStream(objetoTroca.getNomeArquivo() + "");
-                System.out.println("Decriptando arquivo...");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, objetoTroca.getChave());
+            FileOutputStream fout = new FileOutputStream(objetoTroca.getNomeArquivo());
+            FileOutputStream foutCifrado = new FileOutputStream(objetoTroca.getNomeArquivo() + ".md");
+            System.out.println("Decriptando arquivo...");
 
-                byte[] arquivoDecifrado = cipher.doFinal(objetoTroca.getCipherText());
-                System.out.println("Salvando arquivo...");
+            byte[] arquivoDecifrado = cipher.doFinal(objetoTroca.getCipherText());
+            System.out.println("Salvando arquivo...");
 
-                foutCifrado.write(objetoTroca.getCipherText());
-                fout.write(arquivoDecifrado);
+            foutCifrado.write(objetoTroca.getCipherText());
+            fout.write(arquivoDecifrado);
 
-                fout.write(arquivoDecifrado);
-                fout.close();
-                socket.close();
+            fout.write(arquivoDecifrado);
+            fout.close();
+            socket.close();
 
-                System.out.println("Fim da conexão.");
+            System.out.println("Fim da conexão.");
             // }
         }
     }
