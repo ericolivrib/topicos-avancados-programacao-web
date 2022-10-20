9b791f498c6e787bdb7bdbb7ee672043a0adc76f
Érico <oliv.ericorib@gmail.com>
2022-10-20 14:09:08 -0300

    Algoritmo de Diffie Hellman

--- src/main/java/br/ufsm/poli/csi/tapw/criptografia/diffiehellman/Diffie.java
+++ src/main/java/br/ufsm/poli/csi/tapw/criptografia/diffiehellman/Diffie.java
@@ -1,4 +1,48 @@
 package br.ufsm.poli.csi.tapw.criptografia.diffiehellman;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.Random;
+
 public class Diffie {
+
+    public static final int NUM_BITS = 128;
+    private static final Random RANDOM = new SecureRandom();
+
+    public static void main(String[] args) {
+        Diffie diffie = new Diffie();
+        Hellman hellman = new Hellman();
+        diffie.criarChaveCompartilhada(hellman);
+    }
+
+    private void criarChaveCompartilhada(Hellman hellman) {
+        BigInteger[] qa = Util.geraQA(NUM_BITS);
+        BigInteger q = qa[0], a = qa[1];
+
+        BigInteger x = escolherChavePrivada(q, RANDOM);
+        BigInteger y = a.modPow(x, q);
+
+        BigInteger yHellman = hellman.criarChaveCompartilhada(q, a, y);
+        BigInteger k = yHellman.modPow(x, q);
+
+        byte[] kArr = k.toByteArray();
+
+        String secretKey = Base64.getEncoder().encodeToString(kArr);
+
+        System.out.println("Diffie KEY: " + secretKey);
+    }
+
+    public static BigInteger escolherChavePrivada(BigInteger q, Random rnd) {
+        BigInteger n = new BigInteger(NUM_BITS, rnd);
+
+        while (n.compareTo(q) >= 0) {
+            n = new BigInteger(NUM_BITS, rnd);
+        }
+
+        return n;
+    }
+
 }
--- src/main/java/br/ufsm/poli/csi/tapw/criptografia/diffiehellman/Hellman.java
+++ src/main/java/br/ufsm/poli/csi/tapw/criptografia/diffiehellman/Hellman.java
@@ -1,4 +1,28 @@
 package br.ufsm.poli.csi.tapw.criptografia.diffiehellman;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.Random;
+
 public class Hellman {
+
+    private static final Random RANDOM = new SecureRandom();
+
+    public BigInteger criarChaveCompartilhada(BigInteger q, BigInteger a, BigInteger yDiffie) {
+        BigInteger x = Diffie.escolherChavePrivada(q, RANDOM);
+        BigInteger y = a.modPow(x, q);
+
+        // Gera a chave compartilhada
+        BigInteger k = yDiffie.modPow(x, q);
+        byte[] kArr = k.toByteArray();
+
+        String secretKey = Base64.getEncoder().encodeToString(kArr);
+        System.out.println("Hellman KEY: " + secretKey);
+
+        return y;
+    }
+
 }
